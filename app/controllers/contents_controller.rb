f1b373fbf8bbd3404776d202cba61288cc132248
Hiroyuki Kondo <hombredeestemundo@gmail.com>
2011-02-13 08:43:38 +0900

    modify controller: contents_controller

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 class ContentsController < ApplicationController
   def show
-    if params[:id] && File.exist?(path = "#{Rails.root.to_s}/public/contents/#{params[:id]}.html.erb")
+    if params[:id] && File.exist?(path = "#{Rails.root.to_s}/app/views/contents/#{params[:id]}.html.erb")
       render :file => path, :layout => true
     else
       render :text => "Page does not exists.", :status => 404
